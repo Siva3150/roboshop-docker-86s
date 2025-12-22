faf0b6f08ab08458411acc4276dd769fdfad9256
Siva prasad <sivaprasad3150@gmail.com>
2025-12-22 20:12:55 +0530

    k8

--- mysql/custom-script.sh
+++ mysql/custom-script.sh
@@ -10,5 +10,5 @@ fi
 
 export MYSQL_ROOT_PASSWORD=$PASSWORD
 rm -rf /tmp/mysql-root-password.txt
-exec /usr/local/bin/docker-entrypoint.sh mysqld
-#exec /entrypoint.sh mysqld
+exec /entrypoint.sh mysqld
+#exec /usr/local/bin/docker-entrypoint.sh mysqld
